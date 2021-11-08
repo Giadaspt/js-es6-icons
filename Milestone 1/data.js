91d1dcfaa52141cc5c4d38db011a45e571d61f49
Giadaspt <saputo.giada@gmail.com>
2021-11-08 20:11:20 +0100

    small step

--- Milestone 1/data.js
+++ Milestone 1/data.js
@@ -1,4 +1,17 @@
-
+/*
+**Milestone 1**
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
+**Milestone 2**
+Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+**Milestone 3**
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone *(animal, vegetable, user)*. Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+**Consigli del giorno**
+Come sempre, iniziamo prima di tutto dall’analisi e comprensione della consegna. Scomponiamo il problema in micro-passaggi logici che solamente in un secondo momento trasformeremo in codice.
+Le icone presenti nella struttura dati fanno riferimento alla nota libreria **Font Awesome**, perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell’head della pagina.
+Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag `<i>` di un’icona qualsiasi, in particolare **focalizziamoci sulle classi**.
+Come possiamo usare i dati presenti nella nostra struttura dati per creare l’elemento html nel modo corretto e visualizzare l’icona in pagina?
+Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all’interno l’icona e uno span con il nome. Solamente quando la parte logica è completa, ci dedichiamo al css.
+*/
 
 const cards = [
 	{
@@ -115,22 +128,42 @@ const cards = [
 	}
 ];
 
-//document.querySelector('main').innerHTML = '';
+//document.querySelector('.row-card').innerHTML = '';
+allCards(cards)
+function allCards(cards){
 
-document.querySelector('.row-card').innerHTML = `
-<div class="col-2 card d-flex justify-content-end align-items-center m-3">
-          <div class="icons">
-            <i class="${cards.family, cards.prefix}"></i>
-          </div>
-          <p>${cards.name}</p>
-        </div>
-`;
+	let all_card = document.querySelector('.row-card');
+
+	for(let card in cards){
+
+		all_card.innerHTML += `
+		<div class="col-2 card d-flex justify-content-end align-items-center m-3">
+			<div class="icons">
+				<i class="${icon(cards)}"></i>
+			</div>
+			<p>${cards[0].name}</p>
+		</div>
+		`;
+	}
+}
+
 
 //unisco i valori family prefix e name così da crere l'icona
-function icon(card){
-	for(let icon in card){
-		console.log('icona', card[icon]);
+function icon(cards){
+	
+	let final;
+
+	for(let icon in cards){
+
+		let name = cards[icon].name;
+		let family = cards[icon].family;
+		let prefix = cards[icon].prefix;
+
+		final = family +' '+ prefix + name;
+		console.log(final);
 	}
+
+	return final;
 }
 
 
